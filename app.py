be7bf03012e3ea5356c637f620e54986a3185f9c
fmallari <fc.mallari@gmail.com>
2023-06-11 23:52:05 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, jsonify, session
+from boggle import Boggle
+
+boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ZAEDYNO"
+
+boggle_game = Boggle()
+
+@app.route("/")
+def homepage():
+    """SHow board"""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board=board, highscore=highscore, nplays=nplays)
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary"""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score when appropriate"""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
